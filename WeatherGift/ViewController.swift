d51bdb67077372560cf021b901cfc1c9e5a2ee05
Jackie Cochran <cochrajg@bc.edu>
2020-10-06 14:45:06 -0400

    Initial Commit

---
+++ WeatherGift/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeatherGift
+//
+//  Created by Jackie Cochran on 10/6/20.
+//  Copyright © 2020 Jackie Cochran. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
